24940043a4b02b7fe5e56c281fe93d08c49488a5
gabin <gabin.subri@gmail.com>
2022-10-28 17:29:38 +0200

    double type meme couleur

--- script.js
+++ script.js
@@ -1,9 +1,28 @@
 let startIndex = 1;
-let pokemonsNumber = 15;
+let pokemonsNumber = 12;
 
 function createPokemonCard(pokemon) {
   const type = pokemon.types[0].type.name;
   const name = pokemon.name[0].toUpperCase() + pokemon.name.slice(1);
+  
+  if (pokemon.types[1] != undefined){
+    
+
+
+
+    document.querySelector('#pokemonContainer').innerHTML += `
+    <div " style="background:linear-gradient(${grass}, ${poison})" class="pokemon ${type} ${pokemon.types[1].type.name}">
+      <div class="imgContainer">
+        <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${pokemon.id}.png" alt="${name}" />
+      </div>
+      <div class="info">
+        <h3 class="name">${name}</h3>
+        <small class="type">Type: <span>${type} ${pokemon.types[1].type.name}</span></small>
+      </div>
+    <div>
+  `;
+  
+  }else {
 
   document.querySelector('#pokemonContainer').innerHTML += `
     <div class="pokemon ${type}">
@@ -16,7 +35,10 @@ function createPokemonCard(pokemon) {
       </div>
     <div>
   `;
-}
+}}
+fetch(`https://pokeapi.co/api/v2/pokemon/4`).then(response => response.json())
+  .then(data => {
+  console.log(data);})
 
 function fetchPokemons() {
   for (let i = startIndex; i <= pokemonsNumber; i++) {
@@ -24,6 +46,7 @@ function fetchPokemons() {
       .then(response => response.json())
       .then(data => {
         createPokemonCard(data);
+
       });
   }
 }
@@ -36,3 +59,9 @@ document.querySelector('#next').addEventListener('click', function () {
 
 // Initial fetch
 fetchPokemons();
+
+
+
+
+const poison = "#98d7a5";
+const grass = "#defde0";
